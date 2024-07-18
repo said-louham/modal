6274a3c6f6a33c56d4fcbdd5356b9a5b38815464
said-louham <111187783+said-louham@users.noreply.github.com>
2024-07-19 00:32:47 +0100

    init commit

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="isModalOpen=true">open</button>
+   <Modal
+      v-show="isModalOpen"
+      @close-modal="isModalOpen = false"
+      title="Enregisterer !"
+      message="Vos informations ont été enregistrées avec succès !"
+      route="/devis"
+      button="List Devis"
+      :isOpen="isModalOpen"
+    />
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Modal from "~/component/Modal.vue";
+
+const isModalOpen = ref(true)
+</script>
---
+++ component/Modal.vue
@@ -0,0 +1,46 @@
+<template>
+  <Transition
+    enter-active-class="transition-opacity duration-500 ease-out"
+    leave-active-class="transition-opacity duration-500 ease-in"
+    enter-from-class="opacity-0"
+    leave-to-class="opacity-0"
+  >
+    <div v-if="isOpen" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-85" @click="$emit('close-modal')">
+      <div class="relative bg-white h-[400px] w-[500px] rounded-[20px] p-10 text-center" @click.stop>
+        <p>hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</p>
+        <p>hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</p>
+        <p>hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</p>
+      </div>
+      <div class="absolute top-[10%] right-4 cursor-pointer" @click="$emit('close-modal')">
+        <!-- You can add a close icon here if needed -->
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  route: {
+    type: String,
+    required: true
+  },
+  button: {
+    type: String,
+    required: true
+  },
+  isOpen: {
+    type: Boolean,
+    required: true
+  }
+})
+
+defineEmits(['close-modal'])
+</script>
